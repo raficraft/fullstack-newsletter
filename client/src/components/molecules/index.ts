d3f406773475c97100a7cdb0f3dbbad43f643daa
raficraft <raficraft@gmail.com>
2023-07-17 00:43:47 +0200

    bugfix: NewsLetterActions import

--- client/src/components/molecules/index.ts
+++ client/src/components/molecules/index.ts
@@ -2,7 +2,7 @@ import AdminFilter from '@molecules/AdminFilter/AdminFilter';
 import ConfirmAction from '@molecules/ConfirmAction/ConfirmAction';
 import Field from '@molecules/Field/Field';
 import AdminSearch from '@molecules/Form/AdminSearch/AdminSearch';
-import NewslettersActions from '@components/molecules/NewsletterActions/NewsletterActions';
+import NewslettersActions from '@components/molecules/NewsletterActions/NewsLetterActions';
 import Pagination from '@molecules/Pagination/Pagination';
 import Success from '@molecules/Success/Success';
 
